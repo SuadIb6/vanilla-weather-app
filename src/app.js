a4427429bb86f0ddbc6570b2e42450a779201309
SuadIb6 <suadibrahim1020@gmail.com>
2023-05-14 20:54:08 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "b385d498dfa178bfc115dfc214tao005";
-let city = "London";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b385d498dfa178bfc115dfc214tao005&units=metric`;
+function search(city) {
+  let apiKey = "b385d498dfa178bfc115dfc214tao005";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b385d498dfa178bfc115dfc214tao005&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInpuElement = document.querySelector("#city-input");
+  search(cityInpuElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
